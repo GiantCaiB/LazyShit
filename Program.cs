8bd3558c6307f1e0dd0e0019d0aca585e80abc17
Danny Yu <Danny.Yu@globirdenergy.com.au>
2020-10-01 18:59:04 +1000

    init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace LazyAss
+{
+    class Program
+    {
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Point lpPoint);
+        static async Task Main(string[] args)
+        {
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(handle, 6);
+            Console.ReadLine();
+            while (true)
+            {
+                var delayTask = Task.Delay(60000);
+                MakeMove();
+                await delayTask;
+            }
+        }
+
+        static void MakeMove()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(-10, 10);
+            GetCursorPos(out Point lpPoint);
+            SetCursorPos(lpPoint.X + randomNumber, lpPoint.Y + randomNumber);
+        }
+    }
+}
